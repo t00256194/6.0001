45e5f955e2240080a0453e47c8f9b7c0bbb41450
Billy Lian <108542965+t00256194@users.noreply.github.com>
2023-02-17 17:50:49 -0700

    Create ps4a.py

---
+++ ps4/ps4a.py
@@ -0,0 +1,43 @@
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+    #Base Case
+    if len(sequence) == 1:
+        return [sequence]
+    
+    permutations = []
+
+    #Permutations of two characters first, then three characters
+    for let in get_permutations(sequence[1:]):
+        for i in range(len(sequence)):
+            permutations.append(let[:i] + sequence[0] + let[i:])
+    return permutations
+
+
+if __name__ == '__main__':
+
+#    # Put three example test cases here (for your sanity, limit your inputs
+#    to be three characters or fewer as you will have n! permutations for a 
+#    sequence of length n)
+
+    example_input = 'xyz'
+    print('Input:', example_input)
+    print('Expected Output:', ['xyz', 'yxz', 'yzx', 'zxy', 'xzy', 'zyx'])
+    print('Actual Output:', get_permutations(example_input))
